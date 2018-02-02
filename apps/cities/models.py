32649ac92445295db1f9d75cf5c5c3452881bd2d
Pablo González <pdonaire1@gmail.com>
2018-02-02 13:11:47 -0400

    Added cities and stores models

---
+++ apps/cities/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created by: @pdonaire1
+Django settings for store_krato project.
+"""
+from __future__ import unicode_literals
+from django.db import models
+
+class City(models.Model):
+	"""
+	Model Class for cities
+	"""
+	name = models.CharField(max_length=80)
---
+++ apps/stores/models.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created by: @pdonaire1
+Django settings for store_krato project.
+"""
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+from cities.models import City
+
+class Store(models.Model):
+    """
+    Class created for stores
+    """
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return str(self.name) + ' - ' + str(self.id)
+
+
+class UserStore(models.Model):
+    """
+    Class that show the relationship beetween user and stores 
+    and citires
+    """
+    store = models.ForeignKey(Store)
+    user = models.ForeignKey(User)
+    city = models.ForeignKey(City)
+
+    def __str__(self):
+        return str(self.name) + ' - ' + str(self.id)
